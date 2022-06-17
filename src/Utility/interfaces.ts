fbee13002f1febcc55dd0c9648fc131dca4dcacc
otkken2 <lovelovezamakko2@gmail.com>
2022-06-18 08:31:11 +0900

    additional commit(react-hooks-form and small fix)

--- src/Utility/interfaces.ts
+++ src/Utility/interfaces.ts
@@ -1,6 +1,13 @@
+import { Atom } from "jotai";
 import React from "react";
 
 export interface InputProps{
   label: string
   placeholder: string,
+}
+
+export interface serviceSectionInterFace{
+  meansOfTransport: Atom<string>;
+  startPoint: Atom<string>;
+  endPoint: Atom<string>;
 }
