6523423ad8a04b815ba4fed652d736cc9ab3cadb
otkken2 <lovelovezamakko2@gmail.com>
2022-07-13 05:09:47 +0900

    modify HotelChargeSelect UI , Radio to SelectBox

--- src/pages/BusinessTripExpense/HotelChargeRadio.tsx
+++ src/pages/BusinessTripExpense/HotelChargeRadio.tsx
@@ -1,10 +1,12 @@
-import { FormControlLabel, InputAdornment, OutlinedInput, Radio, RadioGroup, styled } from "@mui/material"
+import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, Input, InputAdornment, MenuItem, OutlinedInput, Radio, RadioGroup, Select, styled } from "@mui/material"
+import { margin } from "@mui/system"
+import { ChangeEvent, MouseEventHandler, useEffect, useState } from "react"
 import { Control, Controller, UseFormRegister, UseFormSetValue, UseFormWatch } from "react-hook-form"
 import { StyledInputLabel } from "../../Utility/globalStyles"
 import { Inputs } from "./BusinessTripExpense"
 
 const StyledOutlinedInput = styled(OutlinedInput)({
-
+  marginTop: "10px" 
 })
 
 export const enum HotelCharge{
@@ -51,15 +53,25 @@ export const HotelChargeRadio = (props: HotelChargeRadioProps) => {
   return (
     <>
       <StyledInputLabel>宿泊料</StyledInputLabel>
-      <RadioGroup 
-        onChange={(newValue)=>{
-          props.setValue("hotelChargeType",newValue.target.value as HotelCharge)
-        }} 
-      >
-        <FormControlLabel value={HotelCharge.KOU} control={<Radio {...props.register("hotelChargeType")}/>} label="甲/13000"/>
-        <FormControlLabel value={HotelCharge.OTSU} control={<Radio {...props.register("hotelChargeType")}/>} label="乙/11000"/>
-        <FormControlLabel value={HotelCharge.ACTUAL_HOTEL_CHARGE} control={<Radio {...props.register("hotelChargeType")}/>} label="実費"/>
-      </RadioGroup>
+      <Controller
+        control={props.control}
+        name="hotelChargeType"
+        render={({field})=>
+        (
+          <Select
+            defaultValue=""
+            placeholder="宿泊料を選択"
+            onChange={(newValue)=>{
+              props.setValue("hotelChargeType",newValue.target.value as unknown as HotelCharge)
+            }}
+          > 
+            <MenuItem value="">未選択</MenuItem>
+            <MenuItem value={HotelCharge.KOU}>甲/13000円</MenuItem>
+            <MenuItem value={HotelCharge.OTSU}>乙/11700円</MenuItem>
+            <MenuItem value={HotelCharge.ACTUAL_HOTEL_CHARGE}>実費</MenuItem>
+          </Select>
+        )}
+      />
       {showActualHotelChargeInput()}
     </>
   )
